5a05e210175cd2f03e04a8e740c2cf89b9a7c49e
igm <igm@heurtaux.u-pem.fr>
2019-08-20 10:15:56 +0200

    Revert "Test debug Chessboard"
    This reverts commit f3323cc44e6cb3e0ecc7490a451a4747c5bb27c2.

---
+++ src/gui/RobotWindow.py
@@ -0,0 +1,44 @@
+from tkinter import *
+
+
+class RobotWindow(Toplevel):
+    def __init__(self, mother, root):
+        Toplevel.__init__(self, root)
+        self.geometry("600x400")
+        self.mother = mother
+        self.frame = Frame(self)
+        self.box = Listbox(self)
+        self.box.pack(fill="both", expand=YES)
+        b = Button(self, text="Ok", command=self.print_robot, width=20)
+        u = Button(self, text="Unselect", command=self.unselect, width=20)
+        self.bind('<Escape>', lambda e: self.destroy())
+
+        for i in range(len(self.mother.robotList)):
+            self.box.insert(i, self.mother.robotList[i])
+
+        self.box.pack()
+        u.pack(padx=1, pady=1, side='left')
+        b.pack(padx=1, pady=1, side='right')
+        if not self.mother.robotList:
+            b.configure(state=DISABLED)
+        self.frame.pack()
+
+    def unselect(self):
+        self.mother.label_msg.config(text="Select a Robot")
+        self.mother.currentRobot = None
+        self.mother.canvas.delete("all")
+        self.mother.draw_map()
+        for b in self.mother.button_list:
+            b.configure(state=DISABLED)
+        self.destroy()
+
+    def print_robot(self):
+        self.mother.zoom = 3
+        ind = self.box.curselection()[0]
+        self.mother.currentRobot = self.mother.robotList[ind]
+        self.mother.label_msg.config(text=self.mother.currentRobot.name)
+        for b in self.mother.button_list:
+            b.configure(state=ACTIVE)
+        self.mother.canvas.delete("all")
+        self.mother.draw_map()
+        self.destroy()
---
+++ src/server/Database.py
@@ -0,0 +1,66 @@
+import os.path
+import sqlite3
+
+
+class Database:
+    def __init__(self):
+        if not os.path.isfile("../bdd/fingerPrint.db"):
+            self.bdd = sqlite3.connect('../bdd/fingerPrint.db')
+            self.cmd = self.bdd.cursor()
+            print("Initialising Database...")
+            self.cmd.execute(
+                "create table signals (x integer, y integer, bssid varchar, signal integer not null, type integer not null, PRIMARY KEY(x, y, bssid))")
+        else:
+            self.bdd = sqlite3.connect('../bdd/fingerPrint.db')
+            self.cmd = self.bdd.cursor()
+            print("Database detected")
+
+    def scanFringerprint(self, lines, context):
+        lines = lines.split("\n")
+        for i in range(0, int(len(lines) - 1), 3):
+            addr = lines[i].split(" ")[1]
+            signal = lines[i + 1].split(" ")[1]
+            # name = lines[i+2].split(" ")[1]
+            self.addOnDataBase(context.x, context.y, addr, signal, 0)
+
+    def addOnDataBase(self, x, y, bssid, signal, way):
+        try:
+            signal = int(float(signal))
+            # You have to reconnect the bdd, otherwize you'll get a "SqLite object created in an other Thread error"
+            self.bdd = sqlite3.connect('../bdd/fingerPrint.db')
+            self.cmd = self.bdd.cursor()
+            self.cmd.execute(
+                'insert into signals(x, y, bssid, signal, type) Values (' + str(x) + ', ' + str(y) + ',"' + str(
+                    bssid) + '", ' + str(signal) + ', ' + str(way) + ')')
+            print("db add : ",
+                  '(' + str(x) + ', ' + str(y) + ',"' + str(bssid) + '", ' + str(signal) + ', ' + str(way) + ')')
+            self.bdd.commit()
+        except:
+            print("Triplet X:" + str(x) + " Y:" + str(y) + " BSSID:" + bssid + " already exist.")
+
+    def printTable(self):
+        print("Print table")
+        self.cmd.execute('select * from signals')
+        for i in self.cmd:
+            print(i)
+
+    def valAbs(self, x):
+        return x if x > 0 else -x
+
+    def getScans(self):
+        scans = list()
+        self.bdd = sqlite3.connect('../bdd/fingerPrint.db')
+        self.cmd = self.bdd.cursor()
+        self.cmd.execute('select * from signals')
+        for i in self.cmd:
+            scans.append(i[:-1])
+        return
+
+    def get_fp_list(self):
+        self.bdd = sqlite3.connect('../bdd/fingerPrint.db')
+        self.cmd = self.bdd.cursor()
+        self.cmd.execute('select DISTINCT x, y from signals')
+        lst = []
+        for i in self.cmd:
+            lst.append(i)
+        return lst
